bbc4464a2ebd7cdd98cb1068a0255946fd2b4d09
shinjizen <rhadityahdw3@gmail.com>
2024-09-26 09:42:07 +0700

    initiate map view

--- app/src/main/java/com/dicoding/route_optimization/MainActivity.kt
+++ app/src/main/java/com/dicoding/route_optimization/MainActivity.kt
@@ -1,13 +1,25 @@
 package com.dicoding.route_optimization
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.ArrayAdapter
 import android.widget.AutoCompleteTextView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
 import com.dicoding.route_optimization.databinding.ActivityMainBinding
+import org.osmdroid.config.Configuration
+import org.osmdroid.library.BuildConfig
+import org.osmdroid.tileprovider.tilesource.TileSourceFactory
+import org.osmdroid.util.GeoPoint
+import org.osmdroid.views.CustomZoomButtonsController
+import org.osmdroid.views.MapView
+import org.osmdroid.views.overlay.Marker
+import org.osmdroid.views.overlay.mylocation.GpsMyLocationProvider
+import org.osmdroid.views.overlay.mylocation.MyLocationNewOverlay
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+
+    private lateinit var map: MapView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
@@ -29,5 +41,37 @@ class MainActivity : AppCompatActivity() {
         // Optional: Set how many characters needed before suggestions appear
         autoCompleteTextView.threshold = 1  // Suggest after 1 character is typed
 
+        // konfigurasi map
+        Configuration.getInstance().userAgentValue = BuildConfig.APPLICATION_ID
+
+        map = binding.mapView
+        map.setTileSource(TileSourceFactory.MAPNIK)
+        map.setMultiTouchControls(true)
+        map.zoomController.setVisibility(CustomZoomButtonsController.Visibility.NEVER)
+
+        val mapController = map.controller
+        mapController.setZoom(12.5)
+        val startPoint = GeoPoint(-6.21462, 106.84513)
+        mapController.setCenter(startPoint)
+
+        // marker
+        val marker = Marker(map)
+        marker.position = startPoint
+
+        marker.setAnchor(Marker.ANCHOR_BOTTOM, Marker.ANCHOR_CENTER)
+        marker.title = "You're here"
+
+        map.overlays.add(marker)
+        map.invalidate()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        map.onPause()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        map.onResume()
     }
 }
